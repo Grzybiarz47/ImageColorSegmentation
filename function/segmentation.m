0b69f82f48f1c6d16e6cbc218bc6a3d74b2e1d44
Grzybiarz47 <j.zajda47@gmail.com>
2021-01-03 21:06:23 +0100

    Initial commit

---
+++ function/segmentation.m
@@ -0,0 +1,8 @@
+function result = segmentation(filename)
+% Przykładowa funkcja 
+% jeszcze nie wiem w sumie jak rozdzielimy obliczenia
+image = double(imread(filename))/255;
+image = rgb2gray(image);
+result = image;
+end
+
